318e52717b5e995885fe9641caaad518899390f5
agchxci <agchxci@gmail.com>
2026-09-19 16:48:04 +0100

    test: run WebView tests on Robolectric API 35

--- android/app/src/test/java/xyz/cligmet/app/CligmetWebViewClientTest.kt
+++ android/app/src/test/java/xyz/cligmet/app/CligmetWebViewClientTest.kt
@@ -9,8 +9,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class CligmetWebViewClientTest {
     @Test
     fun internalHttpsStaysInWebView() {
