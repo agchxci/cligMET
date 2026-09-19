ee3c5e55c245b1dad4389edfda0f3fb7f1395774
agchxci <agchxci@gmail.com>
2026-09-19 16:47:59 +0100

    test: run WebView tests on Robolectric API 35

--- android/app/src/test/java/xyz/cligmet/app/MainActivityTest.kt
+++ android/app/src/test/java/xyz/cligmet/app/MainActivityTest.kt
@@ -11,9 +11,11 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 import org.robolectric.Shadows
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class MainActivityTest {
     @Test
     fun webViewUsesRequiredSecuritySettings() {
